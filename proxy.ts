96f341da595b9d289f7cd9f1d4108dc6f216e8bc
victo <victoriagabriellecamargo@gmail.com>
2026-05-17 01:20:52 -0300

    feat: implementação middleware de autenticação para controle de acesso às rotas

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   // Pega o token salvo nos cookies com o nome "session"
   const sessionToken = request.cookies.get("session")?.value;
   const { pathname } = request.nextUrl;
